bfd83f3cf0f07be5af56d80a8a6521b7182956cb
chenchenghung <pihsia.lian@gmail.com>
2022-05-27 09:35:30 +0800

    0527

--- out/production/kuangshen/com/kuang/base/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.kuang.base","c":"Doc","l":"Doc()","u":"%3Cinit%3E()"},{"p":"com.kuang.base","c":"Doc","l":"test(String)","u":"test(java.lang.String)"}];updateSearchResults();
